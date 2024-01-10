697b738974fb1b72b88d57dafba8f6ee6515ef01
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-01-10 12:11:46 -0600

    Completed variable annotation

--- types/variables.ts
+++ types/variables.ts
@@ -22,7 +22,22 @@ isFunny = true;
 isFunny = "asd";
 
 //The Any type, can be used to is you don't want type checking
-let thing: any = "hello";
-thing = 1;
-thing = false;
-thing.toUpperCase();
+//let thing: any = "hello";
+//thing = 1;
+//thing = false;
+//thing.toUpperCase();
+
+let thing = "hello";
+thing().saddads;
+
+
+const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"]
+// This will be type any
+//let foundMovie;
+let foundMovie: string;
+
+for(let movie of movies) {
+  if(movie === "Amadeus") {
+    foundMovie = "Amadeus";
+  }
+}
