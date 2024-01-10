5bd174c3e0c3ed711e1c7b883730ff5478a90f2a
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-01-10 16:13:32 -0600

    Completed nested objects

--- objects/objects.js
+++ objects/objects.js
@@ -1,10 +1,38 @@
 //OBJECTS - can be typed by declaring what the object should look like in the annotation
-var dog = {
-    name: "Link",
-    breed: "Daniff",
-    age: 11
-};
-function printName(person) {
-    console.log("".concat(person.first, " ").concat(person.last));
+//Basic object
+//const dog = {
+//  name: "Link",
+//  breed: "Daniff",
+//  age: 11
+//}
+var coord = { x: 34, y: 2 };
+function doubleCoord(point) {
+    return { x: point.x * 2, y: point.y * 2 };
 }
-printName({ first: "Thomas", last: "Jenkins" });
+//Primitive type alias
+//type MyNum = number;
+//let age: MyNum = 4536;
+// Nested Objects
+// A function with a nested object type parameter
+var describePerson = function (person) {
+    return "Person: ".concat(name, ",\n  Age: ").concat(age, ",\n  Parents: ").concat(parentNames.mom, ", ").concat(parentNames.dad, ".");
+};
+describePerson({ name: 'Jimmy', age: 10, parents: { mom: 'Kim', dad: 'Steve' } });
+var song = {
+    title: "Unchained Melody",
+    artist: "Righteous Brothers",
+    numStreams: 4954983,
+    credits: {
+        producer: "Phil Spector",
+        writer: "Alex North"
+    }
+};
+function calculatePayout(song) {
+    var total = song.numStreams * .0033;
+    return total;
+}
+function printSong(song) {
+    console.log("".concat(song.title, " - ").concat(song.artist));
+}
+calculatePayout(song);
+printSong(song);
--- objects/objects.ts
+++ objects/objects.ts
@@ -1,28 +1,92 @@
 //OBJECTS - can be typed by declaring what the object should look like in the annotation
 //Basic object
-const dog = {
-  name: "Link",
-  breed: "Daniff",
-  age: 11
-}
+//const dog = {
+//  name: "Link",
+//  breed: "Daniff",
+//  age: 11
+//}
 
+/*
 function printName(person: {first: string, last: string}): void {
+
   console.log(`${person.first} ${person.last}`)
 }
 
 printName({first: "Thomas", last: "Jenkins"});
-
+ */
 // Example of type and object on same line
-let coordinate: {x: number, y: number} = {x: 34, y: 2};
+//let coordinate: {x: number, y: number} = {x: 34, y: 2};
 
 // x and y are object type returned by function
-function randomCoord(): {x: number, y: number} {
-  return {x: Math.random(), y: Math.random() };
-}
+//function randomCoord(): {x: number, y: number} {
+//  return {x: Math.random(), y: Math.random() };
+//}
 
 // Can't add age to age because it is not known. It is assumed by TS that anything extra is a "mistake"
-printName({first: "Mick", last: "Jagger", age: 485})
+//printName({first: "Mick", last: "Jagger", age: 485})
 
 //will give no error
-const singer = {first: "Mick", last: "Jagger", age: 485, isAlive: true};
-printName(singer);
+//const singer = {first: "Mick", last: "Jagger", age: 485, isAlive: true};
+//printName(singer);
+
+// TYPE ALIAS
+type Point = {
+  x: number;
+  y: number;
+};
+
+let coord: Point = { x: 34, y: 2 };
+
+function doubleCoord(point: Point): Point {
+  return {x: point.x *2, y: point.y *2};
+}
+
+//Primitive type alias
+//type MyNum = number;
+//let age: MyNum = 4536;
+
+// Nested Objects
+// A function with a nested object type parameter
+const describePerson = (person: {
+  name: string;
+  age: number;
+  parentNames: {
+    mom: string;
+    dad: string;
+  }
+}) => {
+  return `Person: ${name},
+  Age: ${age},
+  Parents: ${parentNames.mom}, ${parentNames.dad}.`;
+}
+
+describePerson({name: 'Jimmy', age: 10, parents: {mom: 'Kim', dad: 'Steve'}});
+
+// More nested object
+type Song = {
+  title: string;
+  artist: string;
+  numStreams: number;
+  credits: { producer: string; writer: string };
+}
+
+const song: Song = {
+  title: "Unchained Melody",
+  artist: "Righteous Brothers",
+  numStreams: 4954983,
+  credits: {
+    producer: "Phil Spector",
+    writer: "Alex North"
+}
+}
+
+function calculatePayout(song: Song): number {
+  let total = song.numStreams * .0033;
+  return total;
+}
+function printSong(song: Song) {
+  console.log(`${song.title} - ${song.artist}`);
+}
+
+calculatePayout(song)
+printSong(song)
